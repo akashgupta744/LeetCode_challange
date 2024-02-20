328ba271751e71f59d1e32a599e7f2deabae747e
akashgupta744 <akgupta427@gmail.com>
2024-02-20 12:49:51 +0530

    missing number in a list

---
+++ lc_20.py
@@ -0,0 +1,9 @@
+def missingNumber(nums):
+    nums.sort()
+    for i in range(len(nums)):
+        if i != nums[i]:
+           return i
+    return len(nums)
+nums = [0,1]
+# nums = [9,6,4,2,3,5,7,0,1]
+print(missingNumber(nums))
