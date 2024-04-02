545d442665fc8cec2c30138f49bc2dc3e8c01dfa
akashgupta744 <akgupta427@gmail.com>
2024-04-02 11:52:19 +0530

    ques 24

---
+++ lc_24.py
@@ -0,0 +1,16 @@
+def isIsomorphic(s,t):
+    d1 = {}
+    d2 = {}
+    cnt = 0
+    for i in range(len(s)):
+        if s[i] not in d1:
+            if t[i] in d2: return False
+            d1[s[i]] = cnt
+            d2[t[i]] = cnt
+            cnt += 1
+        elif t[i] not in d2: return False
+        elif d1[s[i]] != d2[t[i]]: return False
+    return True
+
+s,t = "egg","add"
+print(isIsomorphic(s,t))
